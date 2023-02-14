e2d8f55db24de704176b14d4f6b3ef214e61ce20
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-02-14 10:09:50 +0530

    boilerplate code added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-600 text-center px-4 py-4 font-bold'>
+      App
+    </div>
+  )
+}
+
+export default App
